dad8f26549d0ae8918e2a1b1afc4d0495f9c9f9c
tangweikun <tangweikun@corp.netease.com>
2022-10-18 11:57:27 +0800

    :wrench: update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://tangweikun.github.io/blog2022",
+  url: "https://tangweikun.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
